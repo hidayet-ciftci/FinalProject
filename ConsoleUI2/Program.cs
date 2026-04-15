ee970dea6272a811beb89e0ee47b0d38661b6785
Heyedos <hidosss@hotmail.com>
2026-04-15 09:49:03 +0300

    added wrong DI

--- ConsoleUI2/Program.cs
+++ ConsoleUI2/Program.cs
@@ -14,7 +14,17 @@ namespace ConsoleUI2
             {
                 Console.WriteLine(product.ProductName);
             }
-            
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("DI dependency Injection Mantığı!!");
+            Console.WriteLine("Alltaki productlar sıkı sıkıya bağlı");
+            Console.WriteLine("tight couple -> ürün değiştiği zaman ya da kural , bağlanma şekli manager değiştirmek gerekir");
+            Console.WriteLine("Ama üstteki DI ile bağlandığı zaman içerisine yeni instance göndermek yeterli");
+            productManager.WrongAdd(new Product { CategoryId = 1, ProductName = "sa" });
+            foreach (var pro in productManager.WrongGetAll())
+            {
+                Console.WriteLine(pro.ProductName);
+            }
         }
     }
 }
